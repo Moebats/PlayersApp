d39ed4ccf138e654c185f170594eb21c64c558d6
Rizwan Haider <rizwan.haider@nokia.com>
2017-04-04 14:52:46 -0400

    Initial basic auth with redux support.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import Router from './Router';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBIMcNMb29LqC3-CjxFg81tq6EG3ENyuWU',
+      authDomain: 'cricket-bc128.firebaseapp.com',
+      databaseURL: 'https://cricket-bc128.firebaseio.com',
+      projectId: 'cricket-bc128',
+      storageBucket: 'cricket-bc128.appspot.com',
+      messagingSenderId: '497007993079'
+    });
+  }
+
   render() {
-    return (
-        <View>
-          <Text>Cricket App</Text>
-        </View>
+      const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+      return (
+        <Provider store={store}>
+          <Router />
+        </Provider>
     );
   }
 }
