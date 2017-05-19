dab49e10aa297fea478ca0356c378f392137d7a4
Rizwan Haider <rizwan.haider@nokia.com>
2017-05-19 18:54:15 -0400

    Initial chat work

--- src/components/PlayerItem.js
+++ src/components/PlayerItem.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import { Container, Content, ListItem, Thumbnail, Text, Body, Left, Right, Icon } from 'native-base';
+import { Button, ListItem, Thumbnail, Text, Body, Left, Right, Icon } from 'native-base';
+import { Actions } from 'react-native-router-flux';
 
 
 class PlayerItem extends Component {
 
   render() {
-    const { name, city, position } = this.props.user;
+    const { name, city, position, uid } = this.props.user;
 
     return (
-                <ListItem avatar>
-                    <Left>
-                        <Thumbnail source={require('../images/batsman.png')} />
-                    </Left>
-                    <Body>
-                        <Text>{name}</Text>
-                        <Text note>Player type: {position}</Text>
-                        <Text note>Location: {city}</Text>
-                    </Body>
-                    <Right>
-                        <Icon name='chatboxes' style={{ fontSize: 20, color: '#1ec949' }} />
-                    </Right>
-                </ListItem>
+      <ListItem avatar>
+          <Left>
+              <Thumbnail source={require('../images/batsman.png')} />
+          </Left>
+          <Body>
+              <Text>{name}</Text>
+              <Text note>Player type: {position}</Text>
+              <Text note>Location: {city}</Text>
+          </Body>
+          <Right>
+            <Button
+              onPress={() => Actions.playerChat({ name, uid })}
+              transparent
+            >
+              <Icon name='chatboxes' style={{ fontSize: 20, color: '#1ec949' }} />
+            </Button>
+          </Right>
+      </ListItem>
     );
   }
 }
