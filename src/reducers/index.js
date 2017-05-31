b8324c498581a705aa011c48ae6739fe640f31fe
Mohammad Akhtar <akhtarandroid@gmail.com>
2017-05-30 22:14:06 -0400

    Updated the combined reducers.
    Added georeducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,12 @@ import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
 import SignupReducer from './SignupReducer';
 import PlayerReducer from './PlayerReducer';
+import GeoReducer from './GeoReducer';
+
 
 export default combineReducers({
   auth: AuthReducer,
   signup: SignupReducer,
-  users: PlayerReducer
+  users: PlayerReducer,
+  geo: GeoReducer
 });
